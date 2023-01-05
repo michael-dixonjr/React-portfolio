68f32d9c07c90b83b030cb4944040d837e0a1d34
Michael Dixon Jr <112347876+michael-dixonjr@users.noreply.github.com>
2023-01-05 05:52:27 -0800

    fix

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -17,7 +17,7 @@ const About = ()=>(
         <div className="bio bg-light">
           
           <p style={{fontSize: "29px"}}>
-          I am a certified front end web developer with a bachelor's degree and government experience. I have a strong foundation in HTML, CSS, JavaScript, and React.JS. I am currently learning how to use Node.JS, Next.JS, and JSON, and am always looking to expand my skillset. I am known for my positive attitude and ability to motivate and lift those around me. I thrive on solving problems and finding creative solutions, and obsess over the trial and error process of development. In my free time, I enjoy researching new tech, video games, manga, sci-fi/fantasy, personal development, and walking my dog JoJo. I am excited to bring my passion and skills to any project and contribute to the success of the team and I can guarantee I will make notable contributions to any organization I am a part of.
+          I am a certified front end web developer with a bachelor's degree and government experience. I have a strong foundation in HTML, CSS, JavaScript, and React.JS. I am currently learning how to use Node.JS, Next.JS, and JSON, and I'm always looking to expand my skillset. I am known for my positive attitude and ability to motivate and lift those around me. I thrive on solving problems and finding creative solutions, and obsess over the trial and error process of development. In my free time, I enjoy researching new tech, video games, manga, sci-fi/fantasy, personal development, and walking my dog JoJo. I am excited to bring my passion and skills to any project and contribute to the success of the team and I can guarantee I will make notable contributions to any organization I am a part of.
           </p>
         </div>
 
