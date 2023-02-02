5e9388c943750ca2f902a5ae58afc42a42b9c612
Michael Dixon Jr <112347876+michael-dixonjr@users.noreply.github.com>
2023-02-02 14:27:49 -0800

    adding new portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,6 +3,7 @@ import "../../styles/Portfolio.css"
 import lemonscreenshot from "../../img/lemonscreenshot.png"
 import yggiscreenshot from "../../img/yggiscreenshot.png"
 import portfolioscreenshot from "../../img/portfolioscreenshot.png"
+import Portfolio2Screenshot from "../../img/Portfolio2Screenshot.png"
 
 
 
@@ -19,14 +20,14 @@ const Portfolio = ()=>(
     <div className="cards">
       <div className="card">
         <div className="card-image">
-          <img src={portfolioscreenshot} alt="" height="100%" />
+          <img src={Portfolio2Screenshot} alt="" height="100%" />
         </div>
-        <a href="https://michael-dixonjr.github.io/react-portfolio" target={"_blank"}>
+        <a href="https://michael-dixonjr.github.io/React-Portfolio-2/" target={"_blank"}>
         <div className="card-text">
           <div className="card-text-wrap">
             <p className="card-text-category">Responsinve Web Design</p>
             <h2 className="card-text-title">React.JS Portfolio</h2>
-            <a href="https://github.com/michael-dixonjr/react-portfolio" target={"_blank"}><i className="fab fa-github fa-3x my-2"></i></a>
+            <a href="https://github.com/michael-dixonjr/React-Portfolio-2" target={"_blank"}><i className="fab fa-github fa-3x my-2"></i></a>
           </div>
         </div></a>
       </div>
